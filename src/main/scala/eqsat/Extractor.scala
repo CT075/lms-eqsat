28f7ea5674a74486f30f01c51e55ce09b4b015fa
Cameron Wong <cwong@g.harvard.edu>
2025-09-18 13:29:13 -0400

    begin extractor

--- src/main/scala/eqsat/Extractor.scala
+++ src/main/scala/eqsat/Extractor.scala
@@ -1,4 +1,15 @@
 package eqsat
 
-abstract class Extractor[K] {
+import scala.collection.mutable
+
+abstract class Extractor[K](graph: EGraph[K]) {
+  type Cost
+
+  val costComparator: Ordering[Cost]
+
+  val costs: mutable.Map[EClass[K], Cost] = mutable.Map.empty
+
+  private def totalCost(node: ENode[K]): Option[Cost] = {
+    node.children.foldLeft
+  }
 }
